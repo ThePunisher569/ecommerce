a8eefdba78d4ee142af3d63b9c914feed8e5f568
Aatif <aatifdom10@gmail.com>
2023-09-27 18:31:19 +0530

    Written test cases for login screen, phone input field, otp input field and login button widgets.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,67 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
+import 'package:ecommerce/ui/login/login_screen.dart';
+import 'package:ecommerce/ui/login/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ecommerce/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  /// This test case is to makes sure that all the widgets in the login_screen.dart
+  /// is pumped into widget tree.
+  testWidgets('Login screen widgets test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(
+      home: Login(),
+    ));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    final phoneInputField = find.byType(PhoneInputField);
+    final otpInputField = find.byType(OTPInputField);
+    final loginButton = find.byType(FilledButton);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    expect(phoneInputField, findsOneWidget);
+    expect(otpInputField, findsOneWidget);
+    expect(loginButton, findsOneWidget);
+  });
+
+  testWidgets('Logo widget test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(
+      home: Login(),
+    ));
+
+    final logoImage = find.byType(SvgPicture);
+    final logoText = find.text('Commerce');
+
+    expect(logoImage, findsOneWidget);
+    expect(logoText, findsOneWidget);
+  });
+
+  testWidgets('Phone number validation test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(
+      home: Login(),
+    ));
+
+    // Phone number error validation
+    await tester.enterText(find.byType(PhoneInputField), '3257873');
+
+    // Trigger the login action
+    await tester.tap(find.text('Login'));
+
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Expect a validation error message
+    expect(find.text('Enter a valid 10-digit mobile number'), findsOneWidget);
+  });
+
+  testWidgets('OTP validation test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(
+      home: Login(),
+    ));
+
+    // Enter an invalid OTP (less than 4 digits)
+    await tester.enterText(find.byType(OTPInputField), '456');
+
+    // Trigger the login action
+    await tester.tap(find.text('Login'));
+
+    // Expect a validation error message
+    expect(find.text('Invalid Username or Password'), findsOneWidget);
   });
 }
